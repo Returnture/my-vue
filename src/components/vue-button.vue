eebc54272a994f8a2c77d9391f48f9a28de39f75
赵军 <zhaojun@ec.e-chemchina.com>
2019-01-25 14:20:03 +0800

    first组件

---
+++ src/components/vue-button.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="button">
+        <div class="vButton" :style="{width:width+'px',height:height+'px'}" @click="onClick">
+            <div>{{title}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'vue-button',
+    data () {
+        return {
+            
+        }
+    },
+    props: {
+        width: {
+            type: Number,
+            default: 80
+        },
+        height: {
+            type: Number,
+            default: 40
+        },
+        title: {
+            type: String,
+            default:''
+        }
+    },
+    computed:{
+        
+    },
+    created () {
+        
+    },
+    methods: {
+        onClick(event){
+            this.$emit('onClicked',{
+                event:event
+            });
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .vButton{
+        line-height: 40px;
+        background: #333;
+        color: #ffffff;
+        border-radius: 5px;
+        border: 1px solid black;
+        cursor: pointer;
+    }
+</style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="login">
+        <!-- <input type="text" name="" value="" v-focus> -->
+        <vue-button :width="200" :height="100" :title="'提交'"></vue-button>
+    </div>
+</template>
+
+<script>
+import vueButton from '@/components/vue-button';
+
+export default {
+    name: 'login',
+    data () {
+        return {
+
+        }
+    },
+    components: {
+        vueButton
+    },
+    created () {
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
