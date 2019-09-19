071cf27cc20847bee98243ffc4a75f27018662c5
zhaojun <975809620@qq.com>
2019-09-19 15:54:21 +0800

    注释

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="login">
-        <input type="text" name="" value="" v-focus>
+        <!-- <input type="text" name="" value="" v-focus> -->
         <!-- <vue-button :width="200" :height="100" :title="'提交'"></vue-button> -->
     </div>
 </template>
