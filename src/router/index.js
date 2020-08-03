63f726d1bf1a168ca8753217ac67f58e57ea2df4
zhaoj <zhaoj@jinher.com>
2020-08-03 09:45:09 +0800

    文件夹修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/pages/login'
-import home from '@/pages/home'
+import Attrs from '@/pages/Attrs'
+import Slot from '@/pages/Slot'
 
 Vue.use(Router)
 
-export default new Router({
-    routes: [
-        {
-            path: '/login',
-            name: 'login',
-            component: login
-        },
-        {
-            path: '/',
-            name: 'home',
-            component: home
-        }
-    ]
+const router = new Router({
+  routes: [{
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/Attrs',
+      name: 'Attrs',
+      component: Attrs
+    },
+    {
+      path: '/',
+      name: 'Slot',
+      component: Slot
+    }
+  ]
 })
+
+export default router
