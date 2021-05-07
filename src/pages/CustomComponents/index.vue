1112a9a1c7ac0534a24aaaa90f5f91d1a68dfa7c
Zhao <975809620@qq.com>
2021-05-07 20:00:25 +0800

    update

---
+++ src/pages/CustomComponents/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="">
+        <timer-button :time="3" @click="clickHandler">发送</timer-button>
+        <!-- <TimerButton>发送</TimerButton> -->
+    </div>
+    
+    
+</template>
+
+<script>
+import TimerButton from './timer-button.vue';
+export default {
+    data() {
+        return {
+
+        };
+    },
+    props: {},
+    components: {
+        TimerButton
+    },
+    computed: {},
+    watch: {},
+    methods: {
+        clickHandler(data) {
+            console.log(data);
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+}
+</script>
+<style scoped>
+</style>
---
+++ src/pages/CustomComponents/timer-button.vue
@@ -0,0 +1,53 @@
+<template>
+   <div class="timer-button">
+       <button :disabled="isClick" @click="handler">
+           <slot></slot>
+       </button>
+   </div>
+</template>
+
+<script>
+
+export default {
+name: "timer-button",
+data() {
+   return {
+       isClick: false
+   };
+},
+props: {
+    time: {
+        type: Number,
+        default: 0
+    }
+},
+components: {},
+computed: {},
+watch: {},
+methods: {
+    handler() {
+        this.isClick = true
+        this.$emit("click",this.isClick)
+        setTimeout(()=>{
+            this.isClick = false
+        },this.time*1000)
+    }
+},
+created() {
+
+},
+mounted() {
+
+},
+}
+</script>
+<style scoped>
+    button {
+        width: 80px;
+        height: 40px;
+        background: #999;
+        color: #FFF;
+        text-align: center;
+        line-height: 40px;
+    }
+</style>
